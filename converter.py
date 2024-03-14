8052c1eac08debbc1ed338c0d69a5504f6779bcf
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-03-13 21:20:34 -0300

    Versão assincrona do metodo converter adicionada

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import requests
 from os import getenv
 from fastapi import HTTPException
+import aiohttp
 
 
 ALPHAVANTAGE_APIKEY = getenv('ALPHAVANTAGE_APIKEY')
@@ -10,10 +11,28 @@ def sync_converter(from_currency: str, to_currency: str, price: float):
     
     try:
         response = requests.get(url=url)
+
     except Exception as error:
         raise HTTPException(status_code=400, detail=error)
     
     data = response.json()
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail="Realtime Currency Exchange Rate not in response")
+    
+    exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    
+    return price * exchange_rate
+
+async def async_converter(from_currency: str, to_currency: str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_APIKEY}'
+    
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=url) as response:
+                data =  await response.json()
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
     
     if "Realtime Currency Exchange Rate" not in data:
         raise HTTPException(status_code=400, detail="Realtime Currency Exchange Rate not in response")
--- routers.py
+++ routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from converter import sync_converter
+from converter import sync_converter, async_converter
+from asyncio import gather
 
-router = APIRouter()
+router = APIRouter(prefix='/converter')
 
-@router.get('/converter/{from_currency}')
+@router.get('/{from_currency}')
 def converter(from_currency: str, to_currencies: str, price: float):
     to_currencies = to_currencies.split(',')
     
@@ -18,4 +19,23 @@ def converter(from_currency: str, to_currencies: str, price: float):
         
         result.append(response)
     
+    return result
+
+
+@router.get('/async/{from_currency}')
+async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+    to_currencies = to_currencies.split(',')
+    
+    courotines = []
+    
+    for currency in to_currencies:
+        coro = async_converter(
+            from_currency=from_currency,
+            to_currency = currency,
+            price = price
+        )
+        
+        courotines.append(coro)
+    
+    result = await gather(*courotines)
     return result
