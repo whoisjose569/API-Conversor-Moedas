efa03e43763f92ce756ce53b1944bdcdad2a1753
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-03-27 15:28:41 -0300

    organizando os arquivos

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import router
+from src.routers.routers import router
 
 app = FastAPI()
 app.include_router(router=router)
--- src/routers/routers.py
+++ src/routers/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path, Query
-from converter import sync_converter, async_converter
+from src.config.converter import sync_converter, async_converter
 from asyncio import gather
 
 router = APIRouter(prefix='/converter')
@@ -25,7 +25,9 @@ def converter(from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
 
 
 @router.get('/async/{from_currency}')
-async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+async def async_converter_router(from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$') ,
+                                to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+                                price: float = Query(gt=0)):
     to_currencies = to_currencies.split(',')
     
     courotines = []
