24c43d352ed476e8cc7ecaf6d2601c12fdca2c0f
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-03-27 15:41:21 -0300

    adicionando rota com body parameter

--- src/routers/routers.py
+++ src/routers/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Path, Query
 from config.converter import sync_converter, async_converter
 from asyncio import gather
+from schemas.schemas import ConverterInput
 
 router = APIRouter(prefix='/converter')
 
@@ -29,6 +30,25 @@ async def async_converter_router(from_currency: str = Path(max_length=3, regex='
                                 to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
                                 price: float = Query(gt=0)):
     to_currencies = to_currencies.split(',')
+    
+    courotines = []
+    
+    for currency in to_currencies:
+        coro = async_converter(
+            from_currency=from_currency,
+            to_currency = currency,
+            price = price
+        )
+        
+        courotines.append(coro)
+    
+    result = await gather(*courotines)
+    return result
+
+@router.get('/async/v2/{from_currency}')
+async def async_converter_router(body: ConverterInput,from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$')):
+    to_currencies = body.to_currencies
+    price = body.price
     
     courotines = []
     
