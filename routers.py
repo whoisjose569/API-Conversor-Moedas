dbdd2f6b742078ccd413d896cd34f979e488182f
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-03-27 14:59:12 -0300

    Adicionando validações de path e query parameters

--- routers.py
+++ routers.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 from converter import sync_converter, async_converter
 from asyncio import gather
 
 router = APIRouter(prefix='/converter')
 
 @router.get('/{from_currency}')
-def converter(from_currency: str, to_currencies: str, price: float):
+def converter(from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
+            to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+            price: float = Query(gt=0)):
     to_currencies = to_currencies.split(',')
     
     result = []
